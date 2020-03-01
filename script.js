860608a99781512a2adcfe71c080ac7068b585f7
bwettenstein <wettenstein@gmail.com>
2020-02-29 21:23:30 -0800

    Changed formatting for timer

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const tensMin = document.getElementById('tens-minute');
-const addTensMin = document.getElementById('tens-min-add');
-const subTensMin = document.getElementById('tens-min-sub');
+// const tensMin = document.getElementById('tens-minute');
+// const addTensMin = document.getElementById('tens-min-add');
+// const subTensMin = document.getElementById('tens-min-sub');
 
-addTensMin.addEventListener('click', () => {
-    tensMin.innerHTML = parseInt(tensMin.innerHTML, 10) + 1;
-})
+// addTensMin.addEventListener('click', () => {
+//     tensMin.innerHTML = parseInt(tensMin.innerHTML) + 1;
+// })
 
-subTensMin.addEventListener('click', () => {
-    tensMin.innerHTML = parseInt(tensMin.innerHTML) - 1;
-})
+// subTensMin.addEventListener('click', () => {
+//     tensMin.innerHTML = parseInt(tensMin.innerHTML) - 1;
+// })
